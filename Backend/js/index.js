bdfff2808afc261b25d3dfff48adb99b811d4bdd
unknown <nixodfdf@gmail.com>
2019-04-05 15:04:34 +0100

    Signup/Login php

--- Backend/js/index.js
+++ Backend/js/index.js
@@ -1,4 +1,7 @@
 window.onload = function(){
+
+    var tmp_id;
+
     $('#exampleModal').on('show.bs.modal', function (event) {
           var button = $(event.relatedTarget) // Button that triggered the modal
           var recipient = button.data('whatever') // Extract info from data-* attributes
@@ -17,18 +20,19 @@ window.onload = function(){
                 error: function(err) {
                     console.log(err);
                 }
-            });  
-    })
-    
+            });
+    });
+
+
     $("button#submit").click(function (){
         var sid = $("#studentid").val();
         var pass = $("#password").val();
-        
+
         if($("#password").val()==""||$("#studentid").val()=="")
             $("div#ack").html("Please enter username and password");
         else
             $.ajax ({
-                type:'post',
+                type:'POST',
                 url:'includes/check.php',
                 data: {
                     check: "check",
@@ -38,20 +42,213 @@ window.onload = function(){
                 success:function(response) {
                     if (response==1)
                         {
-                           msg = "Found";
+                           window.location.href = "../index.php";
                         }
-                    else 
+                    else
                         {
                             msg = "not found";
                         }
                     $("#ack").html(msg);
                 },
                  error: function (xhr, ajaxOptions, thrownError) {
-                        
+
                 },
             });
         $("#myForm").submit( function(){
             return false;
+        });
+    });
+
+    $(".likeBtn").click(function (){
+        var obj = this.id;
+        var clsobj = this.className;
+
+        $.ajax ({
+            type: 'POST',
+            url: 'includes/like.php',
+            data: {
+                check: clsobj,
+                P_id: obj
+            },
+
+            success: function(data) {
+                console.log(data);
+            },
+             error: function (xhr, ajaxOptions, thrownError) {
+                   alert(xhr + " - " + ajaxOptions + " - " + thrownError);
+
+            },
+        });
+    });
+
+     $(".DisBtn").click(function (){
+        var obj = this.id;
+        var clsobj = this.className;
+
+        $.ajax ({
+            type: 'POST',
+            url: 'includes/like.php',
+            data: {
+                check: clsobj,
+                P_id: obj
+            },
+
+            success: function(data) {
+                console.log(data);
+            },
+             error: function (xhr, ajaxOptions, thrownError) {
+                   alert(xhr + " - " + ajaxOptions + " - " + thrownError);
+
+            },
+        });
+    });
+
+    $(".Delbtn").click(function (){
+        var obj = this.id;
+        var clsobj = this.className;
+
+        $.ajax ({
+            type: 'POST',
+            url: 'includes/like.php',
+            data: {
+                check:clsobj,
+                P_id: obj
+            },
+
+             success: function(data) {
+                console.log(data);
+                 location.reload();
+            },
+             error: function (xhr, ajaxOptions, thrownError) {
+                   alert(xhr + " - " + ajaxOptions + " - " + thrownError);
+
+            },
+        })
+    });
+
+        $("button#CreateSub").click(function (){
+        var pass = $("#password2").val();
+        var studid = $("#username").val();
+        var email = $("#email").val();
+
+        if(pass==""||studid==""||email=="")
+            $("div#CreateAck").html("Please fill all the details");
+        else {
+            $.ajax ({
+                type:'post',
+                url:'includes/createSQL.php',
+                data: {
+                    check: "check",
+                    password:pass,
+                    username:studid,
+                    email:email
+                },
+                success:function(response) {
+                    if (response==1)
+                        {
+                           msg = "Account created";
+                        }
+                    else
+                        {
+                            msg = "OOF account failed to be created";
+                        }
+                    $("#CreateAck").html(msg);
+                },
+            });
+        }
+        $("#myCreate").submit( function(){
+            return false;
+        });
+    });
+
+    $("button#logout").click(function (){
+        $.ajax ({
+            type:'post',
+            url:'includes/logout.php',
+            success:function(response) {
+                window.location.href = "https://barnumdesigns.xyz/uni/login.php";
+            }
+        })
+    });
+
+    $("button#post_btn").click(function (){
+        var val = $("#post_txt").val();
+        var tit = $("#title_txt").val();
+
+        if (tit==""||val=="")
+            $("#PostAck").html("Please fill all the details");
+        else {
+        $.ajax ({
+            type: 'POST',
+            url:'includes/post.php',
+            data: {
+                check:"insert",
+                txt:val,
+                title:tit
+            },
+
+            success:function(response) {
+                if (response==1)
+                    {
+                        alert("Post made");
+                        location.reload();
+                    } else {
+                        msg = "Failed to create post";
+                    }
+                $("#PostAck").html(msg);
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert(xhr + " - " + ajaxOptions + " - " + thrownError);
+            }
+            });
+        }
+        $("#MyPost").submit( function(){
+            return false;
+        });
+    });
+
+    $("button.Edtbtn").click(function (){
+        tmp_id = this.id;
+
+
+        $.ajax ({
+            type: 'POST',
+            url: 'includes/gather.php',
+            dataType: "json",
+            data: {
+                check: "check",
+                id: tmp_id
+            },
+
+            success:function(response) {
+                document.getElementById("dash").value = response;
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert(xhr + " - " + ajaxOptions + " - " + thrownError);
+                alert(xhr + " - " + ajaxOptions + " - " + thrownError);
+            },
+        });
+    });
+
+    $("#ChngPost").click(function (){
+        var obj = $("#dash").val();
+
+        $.ajax ({
+           type: 'POST',
+            url: 'includes/post.php',
+            data: {
+                check: "alter",
+                id: tmp_id,
+                txt:obj
+            },
+
+            success:function(response) {
+
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert(xhr + " - " + ajaxOptions + " - " + thrownError);
+                alert(xhr + " - " + ajaxOptions + " - " + thrownError);
+            },
         });
     });
 }
--- Backend/login.php
+++ Backend/login.php
@@ -1,21 +1,16 @@
-
-<div class="container">
-    <form id="myForm" action="includes/check.php" method="post">
-        <input name="studentid"  id="studentid" type="text" placeholder="Enter Student ID"/>
-        <input name="password" id="password" type="password" placeholder="Enter Password"/>
-    <button id="submit">Login</button>
-    </form>
-
-    <div id="ack"></div>
-
-    <form id="myCreate">
-        <input name="email" id="email" type="text" placeholder="Student Email"/>
-        <input name="username" id="username" type="text" placeholder="Student ID"/>
-        <input name="password2" id="password2" type="password" placeholder="Enter Password"/>
-        <button id="CreateSub">Create Account</button>
-    </form>
-    <div id="CreateAck"></div>
-</div>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Chris Page">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+    <script src="js/index.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Marble Management Systems</title>
+</head>
 
 <!doctype html>
 <html lang="en">
@@ -29,10 +24,12 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
     <link rel="stylesheet" href="css/stylesheet.css">
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js" integrity="sha384-7aThvCh9TypR7fIc2HV4O/nFMVCBwyIUKL8XCtKE+8xgCgl/PQGuFsvShjr74PBp" crossorigin="anonymous"></script>
+    <script src="js/index.js"></script>
+
     <title>Social Network</title>
   </head>
   <body>
@@ -49,44 +46,48 @@
               <div class="col">
                 <h2>Sign In</h2>
                 <br>
-                <form action="/action_page.php">
+                <form id="myForm" action="includes/check.php" method="post">
                   <div class="form-group">
                     <label for="email">Email/Username:</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                    <input type="text" class="form-control" id="studentid" placeholder="Enter student ID" name="studentud">
                   </div>
                   <div class="form-group">
-                    <label for="pwd">Password:</label>
-                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
                   </div>
                   <div class="form-group form-check">
                     <label class="form-check-label">
                       <input class="form-check-input" type="checkbox" name="remember"> Remember me
                     </label>
                   </div>
-                  <button type="submit" class="btn btn-primary">Sign In</button>
+                  <button id="submit" class="btn btn-primary">Sign In</button>
+
+                  <div id="ack"></div>
                 </form>
               </div>
               <div class="col">
                 <h2>Sign Up</h2>
                 <br>
-                <form action="/action_page.php">
+                <form id="myCreate">
                   <div class="form-group">
-                    <label for="email">Username(Student ID):</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                    <label for="username">Username(Student ID):</label>
+                    <input type="text" class="form-control" id="username" placeholder="Enter student ID" name="username">
                   </div>
                   <div class="form-group">
                     <label for="email">Email:</label>
                     <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
                   </div>
                   <div class="form-group">
-                    <label for="pwd">Password:</label>
-                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+                    <label for="password2">Password:</label>
+                    <input type="password" class="form-control" id="password2" placeholder="Enter password" name="password2">
                   </div>
                   <div class="form-group">
-                    <label for="pwd">Confirm Password:</label>
-                    <input type="password" class="form-control" id="pwd" placeholder="Confirm password" name="pswd">
+                    <label for="password2">Confirm Password:</label>
+                    <input type="password" class="form-control" id="password2" placeholder="Confirm password" name="password2">
                   </div>
-                  <button type="submit" class="btn btn-primary">Sign Up</button>
+                  <button type="submit" class="btn btn-primary" id="CreateSub">Sign Up</button>
+
+                  <div id="CreateAck"></div>
                 </form>
               </div>
             </div>
